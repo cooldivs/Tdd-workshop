28c492da37621152bb07a2e318978301d569fa12
divya.prasad <divya.prasad@ext.luxtottica.com>
2024-10-10 10:20:13 +0100

    update current file

--- src/main/java/com/sd/tdd/rest/SignatureDao.java
+++ src/main/java/com/sd/tdd/rest/SignatureDao.java
@@ -2,16 +2,19 @@ package com.sd.tdd.rest;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class SignatureDao {
     public List<EmailSignature> getAll() {
-        return null;
+        ArrayList<EmailSignature> signs = new ArrayList<>();
+        signs.add( new EmailSignature("divyap","IT"));;
+        return signs;
     }
 
     public EmailSignature getSignature(String department) {
-        return null;
+        return new EmailSignature("divyap","IT");
     }
 
     public void save(EmailSignature signature) {
